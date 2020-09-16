37e9c617e8cf9da8f6441490472665466332a74c
isaac <273070111@qq.com>
2020-09-16 16:21:18 +0800

    [FIX]修改问题

--- src/config/alias.js
+++ src/config/alias.js
@@ -2,19 +2,15 @@ const paths = require('hzero-webpack-scripts/config/paths');
 const path = require('path');
 
 module.exports = {
-  '@common': path.resolve(paths.appRootPath, 'packages', 'sample2-common/lib'),
+  '@common': path.resolve(paths.appRootPath, 'packages', 'sample2-common/src'),
   'hzero-boot-customize-init-config': path.resolve(
     __dirname,
-    '../../packages/sample2-common/lib/config/customize'
+    '../../packages/sample2-common/src/config/customize'
   ),
-  '@/assets': path.resolve(
-    paths.appRootPath,
-    'src/assets'
-  ),
+  '@/assets': path.resolve(paths.appRootPath, 'src/assets'),
   '@': path.resolve(paths.appPath, 'src'),
 
-  components: 'hzero-front/lib/components/',
-  utils: 'hzero-front/lib/utils/',
-  services: 'hzero-front/lib/services/',
-
+  components: 'hzero-front/src/components/',
+  utils: 'hzero-front/src/utils/',
+  services: 'hzero-front/src/services/',
 };

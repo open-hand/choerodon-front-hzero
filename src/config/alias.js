5036f304f58c511d35abb9a617ad4462f97a77b9
isaac <273070111@qq.com>
2020-09-17 11:38:48 +0800

    [FIX]修改问题

--- src/config/alias.js
+++ src/config/alias.js
@@ -5,7 +5,7 @@ module.exports = {
   '@common': path.resolve(paths.appRootPath, 'packages', 'sample2-common/lib'),
   'hzero-boot-customize-init-config': path.resolve(
     __dirname,
-    '../../packages/sample2-common/lib/config/customize'
+    '../../packages/sample2-common/src/config/customize'
   ),
   '@/assets': path.resolve(paths.appRootPath, 'src/assets'),
   '@': path.resolve(paths.appPath, 'src'),
